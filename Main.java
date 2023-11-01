25f01761403d3a64635f7b67cb5088d299450a3f
Jonas <prothetbrasil@gmail.com>
2023-11-01 17:33:29 -0300

    Implementing Matrix.java into Main class for testing

--- Main.java
+++ Main.java
@@ -1,5 +1,13 @@
+import Components.Matrix;
+
 public class Main { 
     public static void main(String[] args) {
+        Matrix mkMatrix = new Matrix();
+        
+        int[][] matrixA = mkMatrix.createMatrix('A');
+        int[][] matrixB = mkMatrix.createMatrix('B');
+
+        mkMatrix.compareMatrix(matrixA,matrixB);     
 
     }
 }
