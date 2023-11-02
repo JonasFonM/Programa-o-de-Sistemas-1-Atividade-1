b9d9afc37f24dbdabfbfa573104414c8a7c72705
Jonas <prothetbrasil@gmail.com>
2023-11-01 21:52:12 -0300

    Finalizing Matrix.java logic

--- Components/Matrix.java
+++ Components/Matrix.java
@@ -7,15 +7,15 @@ public class Matrix {
 
     public int[][] createMatrix(char letra){
         System.out.flush();
-        System.out.println("Informe o número de linhas para a Matriz"+letra+": ");
+        System.out.println("Informe o número de linhas para a Matriz "+letra+": ");
         int l = input.nextInt();
-        System.out.println("Informe o número de colunas para a Matriz"+letra+": ");
+        System.out.println("Informe o número de colunas para a Matriz "+letra+": ");
         int c = input.nextInt();
 
         int matrix[][] = new int[l][c];
         for (int i = 0; i < l; i++) {
             for (int j = 0; j < c; j++) {          
-                System.out.println("Informe o próximo número da Matriz "+letra+":");
+                System.out.println("Informe o próximo número da Matriz "+letra+": ");
                 matrix[i][j] = input.nextInt();
             }
         }
@@ -35,6 +35,27 @@ public class Matrix {
         
     }
 
-    
-    
+    public int[][] multiplyMatrix(int[][] A, int[][] B){
+        int C[][] = new int[A.length][B[0].length];
+        if(B.length == A[0].length) {
+            for (int i = 0; i < A.length; i++) {
+                for (int j = 0; j < B[0].length; j++) {
+                    for (int j2 = 0; j2 < A[0].length; j2++) {
+                        C[i][j] += A[i][j2] * B[j2][j];
+                    }
+                }
+            }
+        }
+        return(C);
+    }       
+
+    public void readMatrix(int[][] M){                       
+        for (int i = 0; i < M.length; i++) {
+            for (int j = 0; j < M[0].length; j++) {
+                System.out.print(M[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
 }
