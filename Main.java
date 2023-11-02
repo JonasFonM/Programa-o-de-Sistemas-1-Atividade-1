e722e5c6f7fae414c2f3744c0444c071dc9d7a49
Jonas <prothetbrasil@gmail.com>
2023-11-01 21:52:27 -0300

    Finalizing Main.java

--- Main.java
+++ Main.java
@@ -1,13 +1,36 @@
 import Components.Matrix;
+import java.util.Scanner;
 
 public class Main { 
     public static void main(String[] args) {
-        Matrix mkMatrix = new Matrix();
+        Scanner input = new Scanner(System.in);
         
-        int[][] matrixA = mkMatrix.createMatrix('A');
-        int[][] matrixB = mkMatrix.createMatrix('B');
+        System.out.println("Você deseja multiplicar um par de Matrizes? \n 1-> Sim\n 2-> Não");
+        int answ = input.nextInt();
+        
+        while (answ == 1) {           
+            Matrix mkMatrix = new Matrix();
+        
+            int[][] matrixA = mkMatrix.createMatrix('A');
+            int[][] matrixB = mkMatrix.createMatrix('B');
 
-        mkMatrix.compareMatrix(matrixA,matrixB);     
+            boolean isPossible = mkMatrix.compareMatrix(matrixA,matrixB);
+        
+            if (isPossible == true) {
+                int[][] matrixC =  mkMatrix.multiplyMatrix(matrixA, matrixB);
 
-    }
+                System.out.println("A multiplicação da Matriz A ");
+                mkMatrix.readMatrix(matrixA);
+                System.out.println("Com a Matriz B ");
+                mkMatrix.readMatrix(matrixB);
+                System.out.println("Resultou na Matriz C ");
+                mkMatrix.readMatrix(matrixC);
+            }
+            
+            System.out.println("Deseja fazer multiplicar um novo par de Matrizes?\n 1-> Sim\n 2-> Não");
+            answ = input.nextInt();
+        }   
+        input.close();
+    } 
+    
 }
